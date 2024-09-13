288dd4963d173ed627e01fed44b1f0ec45ec29ee
tobemo <t.beers.m@gmail.com>
2024-09-13 09:34:19 +0200

    Adds pandas tests.

--- tests/test_vhmm.py
+++ tests/test_vhmm.py
@@ -1,22 +1,25 @@
 from my_vhmm import MyVariationalGaussianHMM
 import pytest
 import numpy as np
+import pandas as pd
 
 
 # earthquake data from http://earthquake.usgs.gov/
-X = np.array([
-    13, 14, 8, 10, 16, 26, 32, 27, 18, 32, 36, 24, 22, 23, 22, 18,
-    25, 21, 21, 14, 8, 11, 14, 23, 18, 17, 19, 20, 22, 19, 13, 26,
-    13, 14, 22, 24, 21, 22, 26, 21, 23, 24, 27, 41, 31, 27, 35, 26,
-    28, 36, 39, 21, 17, 22, 17, 19, 15, 34, 10, 15, 22, 18, 15, 20,
-    15, 22, 19, 16, 30, 27, 29, 23, 20, 16, 21, 21, 25, 16, 18, 15,
-    18, 14, 10, 15, 8, 15, 6, 11, 8, 7, 18, 16, 13, 12, 13, 20,
-    15, 16, 12, 18, 15, 16, 13, 15, 16, 11, 11
-])
+@pytest.fixture
+def X() -> np.ndarray:
+    return np.array([
+        13, 14, 8, 10, 16, 26, 32, 27, 18, 32, 36, 24, 22, 23, 22, 18,
+        25, 21, 21, 14, 8, 11, 14, 23, 18, 17, 19, 20, 22, 19, 13, 26,
+        13, 14, 22, 24, 21, 22, 26, 21, 23, 24, 27, 41, 31, 27, 35, 26,
+        28, 36, 39, 21, 17, 22, 17, 19, 15, 34, 10, 15, 22, 18, 15, 20,
+        15, 22, 19, 16, 30, 27, 29, 23, 20, 16, 21, 21, 25, 16, 18, 15,
+        18, 14, 10, 15, 8, 15, 6, 11, 8, 7, 18, 16, 13, 12, 13, 20,
+        15, 16, 12, 18, 15, 16, 13, 15, 16, 11, 11
+    ])
 
 
 @pytest.fixture
-def trained_model() -> MyVariationalGaussianHMM:
+def trained_model(X) -> MyVariationalGaussianHMM:
     obj = MyVariationalGaussianHMM(n_components=4, n_iter=10)
     obj.fit(X[:, None])
     return obj
@@ -43,8 +46,10 @@ def test_check(trained_model):
         obj._check()
 
 
-def test_fit(trained_model):
+def test_fit(trained_model, X):
     trained_model.predict(X[:, None]) 
+    X = pd.DataFrame(X)
+    trained_model.predict(X)
 
 
 def test_loading_config(trained_model):
@@ -65,7 +70,7 @@ def test_loading_config_model_equality(trained_model):
     assert obj2.transmat_ == pytest.approx(trained_model.transmat_)
 
 
-def test_loading_config_prediction_equality(trained_model):
+def test_loading_config_prediction_equality(trained_model, X):
     cfg = trained_model.get_config()
     obj2 = MyVariationalGaussianHMM.from_config(cfg)
 
@@ -78,7 +83,7 @@ def test_to_json(trained_model):
     string = trained_model.to_json()
 
 
-def test_from_json(trained_model):
+def test_from_json(trained_model, X):
     string = trained_model.to_json()
     obj2 = MyVariationalGaussianHMM.from_json(string)
 
@@ -183,7 +188,7 @@ def test_mapping(trained_model):
     # ! Don't even know if my test is correct
     # hard to wrap my head around mapping over time
 
-def test_mapped_predictions():
+def test_mapped_predictions(X):
     model = MyVariationalGaussianHMM(n_components=4)
     model.n_features = 1
     model.startprob_ = np.array([0.4, 0.3, 0.3])
@@ -205,8 +210,19 @@ def test_mapped_predictions():
     y1 = model.predict(X)
     assert not y1 == pytest.approx(y0)
 
+    X = pd.DataFrame(X)
+    y0 = model.predict(X)
+    model.mapping = np.stack(
+        [
+            [0, 1, 2, 3],
+            [1, 2, 3, 0]
+        ]
+    ).T
+    y1 = model.predict(X)
+    assert not pd.testing.assert_series_equal(y1, y0)
 
-def test_equality(trained_model):
+
+def test_equality(trained_model, X):
     other = MyVariationalGaussianHMM(n_components=trained_model.n_components)
     assert trained_model == trained_model
     assert not other == trained_model
